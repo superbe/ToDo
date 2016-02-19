130ae96419bc0a2877e2178965ad0f1e461c0166
super_be <babarykin@list.ru>
2016-02-19 18:55:36 +0500

    Добавилось не все. Сбор мержа. Добавил подзадачу. Фиксируем.

---
+++ ToDo.Core/Model/SubTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ToDo.Core
+{
+	/// <summary>
+	/// Подзадача.
+	/// </summary>
+	public class SubTask : Task
+	{
+		/// <summary>
+		/// Родительская задача.
+		/// </summary>
+		[DisplayName("Состояние исполнения")]
+		public Guid TaskID { get; set; }
+		public virtual Task Task { get; set; }
+	}
+}
